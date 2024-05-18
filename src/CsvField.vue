277157f2613482fba0d981a50f6f2f2a03cece9b
Nico Hoffmann <me@nhoffmann.com>
2024-05-18 19:38:05 +0200

    Vue improvements

--- src/CsvField.vue
+++ src/CsvField.vue
@@ -1,19 +1,9 @@
 <template>
 	<k-field v-bind="$props" class="k-models-field k-csv-field">
 		<template v-if="!disabled" #options>
-			<k-button
-				v-if="file"
-				:responsive="true"
-				:text="$t('remove')"
-				icon="remove"
-				size="xs"
-				variant="filled"
-				@click="remove(0)"
-			/>
 			<k-button-group
-				v-else
 				ref="buttons"
-				:buttons="buttons"
+				:buttons="file ? button : buttons"
 				layout="collapsed"
 				size="xs"
 				variant="filled"
@@ -22,14 +12,7 @@
 		</template>
 
 		<k-dropzone :disabled="!hasDropzone" @drop="drop">
-			<k-table
-				v-if="rows"
-				:columns="cols"
-				:index="index"
-				:rows="rows"
-				:pagination="pagination ? { ...pagination, details: true } : false"
-				@paginate="preview(pagination.page + 1)"
-			/>
+			<k-table v-if="rows" v-bind="table" @paginate="preview($event.page)" />
 			<k-empty
 				v-else-if="file"
 				:text="$t('field.csv.loading')"
@@ -52,7 +35,6 @@ export default {
 	extends: "k-files-field",
 	props: {
 		columns: Object,
-		limit: Number,
 	},
 	data() {
 		return {
@@ -61,58 +43,64 @@ export default {
 		};
 	},
 	computed: {
-		cols() {
-			if (this.columns) {
-				return Object.fromEntries(
-					Object.keys(this.columns).map((key) => [
-						this.columns[key].key ?? key,
-						this.columns[key],
-					]),
-				);
-			}
-
-			if (this.rows) {
-				return Object.fromEntries(
-					Object.keys(this.rows[0]).map((key) => [key, { label: key }]),
-				);
-			}
-
-			return [];
+		button() {
+			return [
+				{
+					icon: "remove",
+					responsive: true,
+					size: "xs",
+					text: this.$t("remove"),
+					variant: "filled",
+					click: () => this.clear(),
+				},
+			];
 		},
 		file() {
-			return this.value[0];
+			return this.selected[0]?.id;
 		},
-		index() {
-			if (this.limit) {
-				return (this.pagination.page - 1) * this.pagination.limit + 1;
-			}
-
-			return null;
+		table() {
+			return {
+				columns: Object.fromEntries(
+					Object.keys(this.columns ?? this.rows[0] ?? []).map((key) => [
+						this.columns?.[key].key ?? key,
+						this.columns?.[key] ?? { label: key },
+					]),
+				),
+				index: this.pagination
+					? (this.pagination.page - 1) * this.pagination.limit + 1
+					: null,
+				rows: this.rows,
+				pagination: this.pagination
+					? { ...this.pagination, details: true }
+					: false,
+			};
 		},
 	},
-	watch: {
-		file: {
-			async handler() {
-				this.preview();
-			},
-			immediate: true,
-		},
+	mounted() {
+		this.preview();
 	},
 	methods: {
+		clear() {
+			this.selected = [];
+			this.rows = null;
+			this.onInput();
+		},
+		onInput() {
+			this.$emit("input", this.selected);
+			this.preview();
+		},
 		async preview(page = 1) {
 			if (this.file) {
 				const { rows, pagination } = await this.$api.get(
 					this.endpoints.field + "/preview",
 					{
-						file: this.file.id,
+						file: this.file,
 						page,
 					},
 				);
 
 				this.rows = rows;
 				this.pagination = pagination;
-			} else {
-				this.rows = null;
 			}
 		},
 	},
